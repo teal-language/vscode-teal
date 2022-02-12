58130c33f6d3e026d40d256c54382b3a917c9dda
Patrick Desaulniers <patrick.desaulniers36@gmail.com>
2022-02-11 19:31:41 -0500

    Clean up repo

--- client/extension.js
+++ client/extension.js
@@ -1,8 +1,3 @@
-/* --------------------------------------------------------------------------------------------
- * Based on lsp-sample.
- * See LICENSE-vscode-extension-samples at the root of the project for licensing info.
- * ------------------------------------------------------------------------------------------ */
-
 const workspace = require("vscode").workspace;
 const LanguageClient = require("vscode-languageclient/node").LanguageClient;
 
@@ -22,21 +17,17 @@ function activate(context) {
 		debug: executable
 	};
 
-	// Options to control the language client
 	let clientOptions = {
-		// Register the server for .tl files and tlconfig.lua
 		documentSelector: [
 			{ scheme: 'file', language: 'teal' },
 			{ scheme: 'file', language: 'lua', pattern: '**/tlconfig.lua' }
 		],
 		synchronize: {
-			// Notify the server about file changes to .tl and .lua files contained in the workspace
 			fileEvents: workspace.createFileSystemWatcher('**/*.{tl,lua}')
 		},
 		outputChannelName: 'Teal Language Server'
 	};
 
-	// Create the language client and start the client.
 	client = new LanguageClient(
 		'TealLanguageServer',
 		'Teal Language Server',
@@ -44,7 +35,6 @@ function activate(context) {
 		clientOptions
 	);
 
-	// Start the client. This will also launch the server
 	client.start();
 }
 
